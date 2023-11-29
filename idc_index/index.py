8e650229ea3b506dbf323d93ff25f741edd57026
vkt1414 <vkt1414@users.noreply.github.com>
2023-11-29 20:58:29 +0000

    [create-pull-request] automated change

--- idc_index/index.py
+++ idc_index/index.py
@@ -76,7 +76,7 @@ class IDCClient:
         return result_df
 
     def get_idc_version(self):
-        return "v15";
+        return "v16";
     
     def get_collections(self):
         unique_collections = self.index['collection_id'].unique()
